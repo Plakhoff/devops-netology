8c952f419d2d94c4a257bdc07a05ca3fb66d50be
Dmitriy Plakhov <plakhoff0@gmail.com>
2023-12-17 14:25:14 +0300

    edit HW-11

--- terraform/ter-homeworks/02/src/main.tf
+++ terraform/ter-homeworks/02/src/main.tf
@@ -50,9 +50,18 @@ resource "yandex_compute_instance" "platform2" {
     memory        = var.vm_db_memory
     core_fraction = var.vm_db_core_fraction
   }
-}
+
+
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
     }
   }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+}
