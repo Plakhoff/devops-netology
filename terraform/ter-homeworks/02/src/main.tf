e330a84fa7e8629e182736ee6194a800e053dc0b
Dmitriy Plakhov <plakhoff0@gmail.com>
2023-12-17 12:36:07 +0300

    edit HW-3

--- terraform/ter-homeworks/02/src/main.tf
+++ terraform/ter-homeworks/02/src/main.tf
@@ -10,15 +10,15 @@ resource "yandex_vpc_subnet" "develop" {
 
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_yandex_compute_image
 }
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standard-v1"
+  name        = var.vm_web_yandex_compute_instance
+  platform_id = var.vm_web_platform_id
   resources {
-    cores         = 2
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vm_web_cores
+    memory        = vm_web_memory
+    core_fraction = var.vm_web_core_fraction
   }
   boot_disk {
     initialize_params {
